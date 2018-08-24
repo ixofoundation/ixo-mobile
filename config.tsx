3c5c54c2de30b903089e5f2336e164e942b7c28d
Müller <mullerfourie@gmail.com>
2018-08-24 17:05:13 +0200

    Update config.tsx

--- config.tsx
+++ config.tsx
@@ -1,4 +1,4 @@
 export const env = {
     REACT_APP_BLOCKCHAIN_IP: "https://blockchain_uat.ixo.world",
-    REACT_APP_BLOCK_SYNC_URL: "https://explorer_uat.ixo.world"
+    REACT_APP_BLOCK_SYNC_URL: "https://explorer_uat.ixo.world",
 }
