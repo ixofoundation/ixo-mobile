4cd65387c34406809a331c2ffe2b86950feb74b1
mullerfourie <mullerfourie@gmail.com>
2018-08-24 16:07:33 +0200

    Updated config file to point to uat

--- config.tsx
+++ config.tsx
@@ -1,13 +1,5 @@
 export const env = {
     REACT_IXO_LOCAL_ENV_IP: "192.168.1.253",
-    REACT_APP_BLOCKCHAIN_IP: "http://192.168.1.253:46657",
-    REACT_APP_BLOCK_SYNC_URL: "http://192.168.1.253:8080",
-    // REACT_APP_BLOCKCHAIN_IP: "http://beta.cosmos.ixo.world:46657",
-    // REACT_APP_BLOCK_SYNC_URL: "http://beta.cosmos.ixo.world",
-    // REACT_APP_BLOCKCHAIN_IP: "35.192.187.110",
-    // REACT_APP_BLOCK_SYNC_URL: "https://ixo-block-sync.herokuapp.com",
-
-    // NICK IP
-    // REACT_APP_BLOCKCHAIN_IP: "http://192.168.1.115:46657",
-    // REACT_APP_BLOCK_SYNC_URL: "http://192.168.1.115:8080",
+    REACT_APP_BLOCKCHAIN_IP: "https://blockchain_uat.ixo.world",
+    REACT_APP_BLOCK_SYNC_URL: "https://explorer_uat.ixo.world",
 }
