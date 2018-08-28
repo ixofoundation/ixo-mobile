0ece2ced378321107737f4fa75f58bd6c50ddef0
Nicolaas0411 <nicolaas.vercuiel73@gmail.com>
2018-08-28 11:29:30 +0200

    # Working expo-detach

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp9ade99d90e794eaa9f6b35049faa44f2://192.168.1.115:19000";
+
+}
