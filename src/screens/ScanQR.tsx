f46a2295b4aa3f14d8678c84645e6bbd51ed1196
Nicolaas0411 <nicolaas.vercuiel73@gmail.com>
2018-09-17 10:04:41 +0200

    # Refactored QR scan.

--- src/screens/ScanQR.tsx
+++ src/screens/ScanQR.tsx
@@ -16,6 +16,7 @@ import { initUser } from '../redux/user/user_action_creators';
 import { ThemeColors } from '../styles/Colors';
 import ModalStyle from '../styles/Modal';
 import { Decrypt, generateSovrinDID, getSignature } from '../utils/sovrin';
+import validator from 'validator';
 
 const keysafelogo = require('../../assets/keysafe-logo.png');
 const { height, width } = Dimensions.get('window');
@@ -40,13 +41,13 @@ interface State {
 	qrFound: boolean;
 	loading: boolean;
 	modalVisible: boolean;
-	password: string;
+	password: string | null;
 	revealPassword: boolean;
 	payload: IMnemonic | null;
 	errors: boolean;
-	projectTitle: string;
-	projectDid: string;
-	serviceEndpoint: string;
+	projectTitle: string | null;
+	projectDid: string | null;
+	serviceEndpoint: string | null;
 }
 
 export interface Props extends ParentProps, DispatchProps, StateProps {}
@@ -68,13 +69,13 @@ export class ScanQR extends React.Component<Props, State> {
 		qrFound: false,
 		loading: false,
 		modalVisible: false,
-		password: '',
+		password: null,
 		revealPassword: true,
 		payload: null,
 		errors: false,
-		projectTitle: '',
-		projectDid: '',
-		serviceEndpoint: ''
+		projectTitle: null,
+		projectDid: null,
+		serviceEndpoint: null
 	};
 
 	async componentWillMount() {
@@ -86,32 +87,26 @@ export class ScanQR extends React.Component<Props, State> {
 		this.props.onIxoInit();
 	}
 
-	resetProjectStateVars() {
-		this.setState({ projectTitle: '', serviceEndpoint: '', projectDid: '' });
-	}
-
 	_handleBarCodeRead = (payload: any) => {
 		if (!this.state.modalVisible) {
-			this.setState({ modalVisible: true, payload: payload.data });
-		}
-
-		if (payload.data.includes('projects')) {
-			const projectDid = payload.data.substring(payload.data.length - 39, payload.data.length - 9);
-			this.setState({ projectDid: projectDid });
-			this.props.ixo.project.getProjectByProjectDid(projectDid).then((project: any) => {
-				this.setState({ projectTitle: project.data.title, serviceEndpoint: project.data.serviceEndpoint });
-			});
-		} else {
-			this.resetProjectStateVars();
+			if (validator.isBase64(payload.data)) {
+				this.setState({ modalVisible: true, payload: payload.data });
+			} else if (payload.data.includes('projects')) {
+				const projectDid = payload.data.substring(payload.data.length - 39, payload.data.length - 9);
+				this.setState({ modalVisible: true, payload: null, projectDid: projectDid });
+				this.props.ixo.project.getProjectByProjectDid(projectDid).then((project: any) => {
+					this.setState({ projectTitle: project.data.title, serviceEndpoint: project.data.serviceEndpoint });
+				});
+			}
 		}
 	};
 
 	handleButtonPress = () => {
 		if (this.state.payload && this.state.password) {
 			try {
-				const mnemonicJson: IMnemonic = Decrypt(this.state.payload, this.state.password);
+				const mnemonicJson: IMnemonic = Decrypt(this.state.payload, this.state.password!);
 				SecureStore.setItemAsync(SecureStorageKeys.encryptedMnemonic, this.state.payload!);
-				SecureStore.setItemAsync(SecureStorageKeys.password, this.state.password);
+				SecureStore.setItemAsync(SecureStorageKeys.password, this.state.password!);
 				AsyncStorage.setItem(LocalStorageKeys.firstLaunch, 'true');
 
 				let user: IUser = {
@@ -137,7 +132,6 @@ export class ScanQR extends React.Component<Props, State> {
 				agentDid: this.props.user!.did,
 				projectDid: this.state.projectDid
 			};
-
 			getSignature(agentData).then((signature: any) => {
 				this.props.ixo.agent.createAgent(agentData, signature, this.state.serviceEndpoint).then((res: any) => {
 					console.log('Response: ' + JSON.stringify(res));
@@ -147,7 +141,7 @@ export class ScanQR extends React.Component<Props, State> {
 							type: 'danger',
 							position: 'top'
 						});
-						this.navigateToProjects();       
+						this.navigateToProjects();
 					} else {
 						Toast.show({
 							text: `Successfully registered as ${agentData.role}`,
@@ -172,16 +166,12 @@ export class ScanQR extends React.Component<Props, State> {
 		);
 	}
 
-	handleResetScan = () => {
-		this.setState({ password: '', modalVisible: false, payload: null, errors: false });
+	resetStateVars = () => {
+		this.setState({ modalVisible: false, password: null, payload: null, errors: false, projectDid: null, projectTitle: null, serviceEndpoint: null });
 	};
 
-	setModalVisible(visible: boolean) {
-		this.setState({ modalVisible: visible });
-	}
-
 	renderDescriptionText() {
-		if (this.state.projectDid) {
+		if (this.state.projectDid !== null) {
 			return (
 				<View style={ModalStyle.flexLeft}>
 					<Text style={{ color: ThemeColors.white, fontSize: 15 }}>{this.props.screenProps.t('connectIXOComplete:projectInformation')}</Text>
@@ -197,7 +187,7 @@ export class ScanQR extends React.Component<Props, State> {
 	}
 
 	renderPasswordField() {
-		if (!this.state.projectDid) {
+		if (this.state.projectDid === null) {
 			return (
 				<View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, alignItems: 'center' }}>
 					<Image resizeMode={'contain'} style={{ width: width * 0.06, height: width * 0.06, position: 'absolute', top: width * 0.06 }} source={keysafelogo} />
@@ -234,11 +224,11 @@ export class ScanQR extends React.Component<Props, State> {
 					<View style={ModalStyle.modalOuterContainer}>
 						<View style={ModalStyle.modalInnerContainer}>
 							<View style={ModalStyle.flexRight}>
-								<Icon onPress={() => this.setModalVisible(false)} active name="close" style={{ color: ThemeColors.white, top: 10, fontSize: 30 }} />
+								<Icon onPress={() => this.resetStateVars()} active name="close" style={{ color: ThemeColors.white, top: 10, fontSize: 30 }} />
 							</View>
 							<View style={ModalStyle.flexLeft}>
 								<Text style={{ color: ThemeColors.blue_lightest, fontSize: 29 }}>
-									{this.state.projectDid
+									{this.state.projectDid !== null
 										? this.props.screenProps.t('connectIXOComplete:registerAsServiceAgent')
 										: this.props.screenProps.t('connectIXOComplete:scanSuccessful')}
 								</Text>
@@ -250,7 +240,7 @@ export class ScanQR extends React.Component<Props, State> {
 							<LightButton
 								onPress={() => this.handleButtonPress()}
 								text={
-									this.state.projectDid
+									this.state.projectDid !== null
 										? this.props.screenProps.t('connectIXOComplete:registerButtonText')
 										: this.props.screenProps.t('connectIXOComplete:unlockButtonText')
 								}
@@ -263,20 +253,20 @@ export class ScanQR extends React.Component<Props, State> {
 		return (
 			<View style={ModalStyle.modalOuterContainer}>
 				<View style={ModalStyle.modalInnerContainer}>
-						<View style={ModalStyle.flexRight}>
-							<Icon onPress={() => this.setModalVisible(false)} active name="close" style={{ color: ThemeColors.white, top: 10, fontSize: 30 }} />
-						</View>
-						<View style={ModalStyle.flexLeft}>
-							<Text style={{ color: ThemeColors.blue_lightest, fontSize: 29 }}>Scan unsuccessful</Text>
-						</View>
-						<View style={ModalStyle.divider} />
-						<View style={ModalStyle.flexLeft}>
-							<Text style={{ color: ThemeColors.white, fontSize: 15 }}>There has been an error connecting to the ixo Key Safe</Text>
-						</View>
-						<LightButton onPress={() => this.handleResetScan()} text={this.props.screenProps.t('scanQR:rescan')} />
-						<TouchableOpacity onPress={() => this.props.navigation.dispatch(registerAction)}>
-							<Text style={{ color: ThemeColors.blue_lightest, fontSize: 15, textDecorationLine: 'underline', textAlign: 'center' }}>Are you registered?</Text>
-						</TouchableOpacity>
+					<View style={ModalStyle.flexRight}>
+						<Icon onPress={() => this.setModalVisible(false)} active name="close" style={{ color: ThemeColors.white, top: 10, fontSize: 30 }} />
+					</View>
+					<View style={ModalStyle.flexLeft}>
+						<Text style={{ color: ThemeColors.blue_lightest, fontSize: 29 }}>Scan unsuccessful</Text>
+					</View>
+					<View style={ModalStyle.divider} />
+					<View style={ModalStyle.flexLeft}>
+						<Text style={{ color: ThemeColors.white, fontSize: 15 }}>There has been an error connecting to the ixo Key Safe</Text>
+					</View>
+					<LightButton onPress={() => this.resetStateVars()} text={this.props.screenProps.t('scanQR:rescan')} />
+					<TouchableOpacity onPress={() => this.props.navigation.dispatch(registerAction)}>
+						<Text style={{ color: ThemeColors.blue_lightest, fontSize: 15, textDecorationLine: 'underline', textAlign: 'center' }}>Are you registered?</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 		);
