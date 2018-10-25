ff40f838d70eebfc509b35bbf227b2245788bfbc
Müller <mullerfourie@gmail.com>
2018-10-25 10:59:02 +0200

    config.tsx update

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,4 @@
 export const env = {
-	// REACT_APP_BLOCKCHAIN_IP: 'http://blockchain_uat.ixo.world',
-	// REACT_APP_BLOCK_SYNC_URL: 'https://exploreruat.ixo.world'
-	// REACT_APP_BLOCKCHAIN_IP: 'http://blockchain_uat.ixo.world',
-	// REACT_APP_BLOCK_SYNC_URL: 'https://exploreruat.ixo.world',
-	REACT_APP_BLOCKCHAIN_IP: 'http://192.168.1.253:5000',
-	REACT_APP_BLOCK_SYNC_URL: 'http://192.168.1.253:8080'
+	REACT_APP_BLOCKCHAIN_IP: 'http://blockchain_uat.ixo.world',
+	REACT_APP_BLOCK_SYNC_URL: 'https://exploreruat.ixo.world'
 };
